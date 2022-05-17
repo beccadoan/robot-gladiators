92bfce4332e9cab40746db9d9d5cfb4cd0bc0240
Becca Doan <rebecca.doan@yahoo.com>
2022-05-16 20:04:44 -0500

    added functionality to fight multiple enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,22 @@
-// git var playerName = window.prompt("What is your robot's name?");
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
+
+var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+
+var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators!")
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
@@ -62,4 +70,8 @@ var fight = function() {
 
 };
 
-// fight();
+//  fight();
+
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
