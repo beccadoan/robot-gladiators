784640955b33b5ef809267336f6cb77b3fa21d40
Becca Doan <rebecca.doan@cirrus.com>
2022-05-20 14:54:06 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,6 +4,9 @@
 //    * Defeat each enemy-robot
 // "LOSE" - Player robot's health is zero or less
 
+var randomNumber = function(min,max){
+  return Math.floor(Math.random()*(max-min+1) + min);
+}
 
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
@@ -12,8 +15,10 @@ var playerMoney = 10;
 
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
+var enemyHealth = randomNumber(40,60);
 var enemyAttack = 12;
+
+
 
 
 var fight = function(enemyName) {
@@ -30,14 +35,15 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0,playerMoney - 10);
         console.log("playerMoney", playerMoney)
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -55,7 +61,8 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage =randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -84,10 +91,11 @@ var startGame = function() {
     }
     else {
       window.alert("You have lost your robot in battle! Game Over!");
+      return;
       break;
     }
   }
   startGame();
 }
 
-//startGame();
+startGame();
